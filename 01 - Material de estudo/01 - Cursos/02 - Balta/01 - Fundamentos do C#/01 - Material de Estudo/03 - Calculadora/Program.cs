e74ff96b9909ffb0edc506ca9f6ad2af28ad1c74
AlessandroC.Santos <santoscarvalhoalessandro@gmail.com>
2023-09-29 23:04:06 -0300

    Subindo estudo de fundamentos de c#

--- 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/03 - Calculadora/Program.cs
+++ 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/03 - Calculadora/Program.cs
@@ -7,6 +7,18 @@ class Program
         Console.Clear();
         Console.WriteLine("Primeiro valor: ");
         float v1 = float.Parse(Console.ReadLine());
-        Console.WriteLine(v1);
+
+        Console.WriteLine("Segundo valor: ");
+        float v2 = float.Parse(Console.ReadLine());
+        
+
+        Console.WriteLine("");
+
+        float resultado = v1+v2;
+        Console.WriteLine("O resultado da soma é " + resultado);
+        Console.WriteLine("O resultado da soma é " + (v1 + v2));
+        Console.WriteLine($"O resultado da soma é {resultado}");
+        Console.WriteLine($"O resultado da soma é {v1+v2}");
+
     }
 }
