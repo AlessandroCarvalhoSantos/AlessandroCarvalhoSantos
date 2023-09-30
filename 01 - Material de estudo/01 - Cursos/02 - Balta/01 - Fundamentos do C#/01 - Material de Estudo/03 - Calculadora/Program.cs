1ce7c6021369e8303ddbc922b61d2099a08c7552
AlessandroC.Santos <santoscarvalhoalessandro@gmail.com>
2023-09-30 13:46:42 -0300

    Subindo atualização de estudo do curso balta, subtração calculadora

--- 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/03 - Calculadora/Program.cs
+++ 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/03 - Calculadora/Program.cs
@@ -3,9 +3,11 @@
 
 class Program
 {
-    static void Main(string[] args){
+    static void Main(string[] args)
+    {
    
         Soma();
+        Subtracao();
     }
 
     static void Soma(){
@@ -24,5 +26,25 @@ class Program
         Console.WriteLine("O resultado da soma é " + (v1 + v2));
         // Console.WriteLine($"O resultado da soma é {resultado}");
         // Console.WriteLine($"O resultado da soma é {v1+v2}");
+
+        Console.WriteLine(Console.ReadKey());
+    }
+
+    static void Subtracao()
+    {
+        Console.Clear();
+        
+        Console.WriteLine("Primeiro valor: ");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Segundo valor: ");
+        float v2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+
+        float resultado = v1-v2;
+        Console.WriteLine($"O resultado da subtração é {resultado}");
+
+        Console.WriteLine(Console.ReadKey());
     }
 }
