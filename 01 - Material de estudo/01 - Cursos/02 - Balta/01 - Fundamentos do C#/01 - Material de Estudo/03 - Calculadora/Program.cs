9a620622f391c2508607ffa8ed6ae22e0954972d
AlessandroC.Santos <santoscarvalhoalessandro@gmail.com>
2023-09-30 13:06:02 -0300

    Atualização de estudo do balta, criando uma calculadora

--- 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/03 - Calculadora/Program.cs
+++ 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/03 - Calculadora/Program.cs
@@ -4,6 +4,11 @@
 class Program
 {
     static void Main(string[] args){
+   
+        Soma();
+    }
+
+    static void Soma(){
         Console.Clear();
         Console.WriteLine("Primeiro valor: ");
         float v1 = float.Parse(Console.ReadLine());
@@ -15,10 +20,9 @@ class Program
         Console.WriteLine("");
 
         float resultado = v1+v2;
-        Console.WriteLine("O resultado da soma é " + resultado);
+        // Console.WriteLine("O resultado da soma é " + resultado);
         Console.WriteLine("O resultado da soma é " + (v1 + v2));
-        Console.WriteLine($"O resultado da soma é {resultado}");
-        Console.WriteLine($"O resultado da soma é {v1+v2}");
-
+        // Console.WriteLine($"O resultado da soma é {resultado}");
+        // Console.WriteLine($"O resultado da soma é {v1+v2}");
     }
 }
