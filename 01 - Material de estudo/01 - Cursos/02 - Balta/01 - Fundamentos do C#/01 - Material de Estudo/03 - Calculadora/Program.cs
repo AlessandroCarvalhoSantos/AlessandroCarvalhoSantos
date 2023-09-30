31745a4fa48b97a2b36a994faa56080509c530db
AlessandroC.Santos <santoscarvalhoalessandro@gmail.com>
2023-09-30 18:11:56 -0300

    Atualização de estudo, curso balta calculadora divisão

--- 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/03 - Calculadora/Program.cs
+++ 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/03 - Calculadora/Program.cs
@@ -8,6 +8,7 @@ class Program
    
         Soma();
         Subtracao();
+        Divisao();
     }
 
     static void Soma(){
@@ -46,5 +47,22 @@ class Program
         Console.WriteLine($"O resultado da subtração é {resultado}");
 
         Console.WriteLine(Console.ReadKey());
+    }
+
+    static void Divisao()
+    {
+        Console.Clear();
+
+        Console.WriteLine("Primeiro valor:");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Segundo valor:");
+        float v2 = float.Parse(Console.ReadLine());
+        
+        Console.WriteLine("");
+
+        float resultado = v1/v2;
+        Console.WriteLine($"O resultado da divisão é {resultado}");
+        Console.ReadKey();
     }
 }
