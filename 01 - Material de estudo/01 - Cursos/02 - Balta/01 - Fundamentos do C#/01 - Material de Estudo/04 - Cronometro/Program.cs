f8fa8e54eb01c5e07aecba38713a79f10289737c
AlessandroC.Santos <santoscarvalhoalessandro@gmail.com>
2023-10-01 15:27:33 -0300

    Atualização de estudo do curso balta, criando funcao start do cronometro

--- 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/04 - Cronometro/Program.cs
+++ 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/04 - Cronometro/Program.cs
@@ -6,4 +6,16 @@ class Program
     {
 
     }
+
+    static void Start()
+    {
+        int time = 10;
+        int currentTime = 0;
+
+        while(currentTime != time)
+        {
+            currentTime++;
+        }
+    }
+
 }
