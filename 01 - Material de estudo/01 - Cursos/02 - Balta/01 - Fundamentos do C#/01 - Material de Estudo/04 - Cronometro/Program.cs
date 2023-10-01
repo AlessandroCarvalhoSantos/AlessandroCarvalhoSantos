322ea233aa4a76798825eaa30044e52086603012
AlessandroC.Santos <santoscarvalhoalessandro@gmail.com>
2023-10-01 17:06:47 -0300

    Atualização de estudo do curso balta, tratando o tempo do cronômetro

--- 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/04 - Cronometro/Program.cs
+++ 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/04 - Cronometro/Program.cs
@@ -20,7 +20,7 @@ class Program
         string data = Console.ReadLine().ToLower();
 
         char type = char.Parse(data.Substring(data.Length-1,1));
-
+        int time = int.Parse(data.Substring(0, data.Length-1));
     }
 
     static void Start(int time)
