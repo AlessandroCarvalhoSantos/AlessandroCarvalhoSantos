88d2e53c191778df6d9d508f84855f41f9305d99
AlessandroC.Santos <santoscarvalhoalessandro@gmail.com>
2023-10-07 21:18:19 -0300

    Atualização de estudo do curso do balta, desenhando o menu

--- 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/08 - Editor HTML/Menu.cs
+++ 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/08 - Editor HTML/Menu.cs
@@ -8,6 +8,9 @@ public static class Menu
         Console.BackgroundColor = ConsoleColor.Blue;
         Console.ForegroundColor = ConsoleColor.Black;
         DrawScreen();
+        WriteOptions();
+
+        var option = short.Parse(Console.ReadLine());
     }
 
     public static void DrawScreen()
@@ -35,5 +38,23 @@ public static class Menu
             Console.Write("-");
 
         Console.WriteLine("+");
+    }
+
+    public static void WriteOptions()
+    {
+        Console.SetCursorPosition(3,2);
+        Console.WriteLine("Editor HTML");
+        Console.SetCursorPosition(3,3);
+        Console.WriteLine("======================");
+        Console.SetCursorPosition(3,4);
+        Console.WriteLine("Selecione uma opção abaixo:");
+        Console.SetCursorPosition(3,6);
+        Console.WriteLine("1 - Novo arquivo");
+        Console.SetCursorPosition(3,7);
+        Console.WriteLine("2 - Abrir");
+        Console.SetCursorPosition(3,9);
+        Console.WriteLine("0 - Sair");
+        Console.SetCursorPosition(3,10);
+        Console.Write("Opção: ");
     }
 }
