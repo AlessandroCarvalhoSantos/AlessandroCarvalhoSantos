6009223f0e350134b8dc490ecdc508e49ca8e48f
AlessandroC.Santos <santoscarvalhoalessandro@gmail.com>
2023-10-07 21:02:31 -0300

    Atualização de estudo do curso do balta, desenhando no terminal

--- 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/08 - Editor HTML/Menu.cs
+++ 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/08 - Editor HTML/Menu.cs
@@ -4,6 +4,36 @@ public static class Menu
 {
     public static void Show()
     {
-        Menu.Show();
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.ForegroundColor = ConsoleColor.Black;
+        DrawScreen();
+    }
+
+    public static void DrawScreen()
+    {
+        Console.Write("+");
+
+        for(int i = 0; i<= 30; i++)
+            Console.Write("-");
+
+        Console.WriteLine("+");
+
+        for(int lines = 0; lines<= 10; lines++)
+        {
+            Console.Write("|");
+            for(int i = 0; i<= 30; i++)
+                Console.Write(" ");
+            
+            Console.Write("|");
+            Console.Write("\n");
+        }
+
+        Console.Write("+");
+
+        for(int i = 0; i<= 30; i++)
+            Console.Write("-");
+
+        Console.WriteLine("+");
     }
 }
--- 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/08 - Editor HTML/Program.cs
+++ 01 - Material de estudo/01 - Cursos/02 - Balta/01 - Fundamentos do C#/01 - Material de Estudo/08 - Editor HTML/Program.cs
@@ -4,10 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Clear();
-        Console.BackgroundColor = ConsoleColor.Blue;
-        Console.ForegroundColor = ConsoleColor.Black;
-        
-
+        Menu.Show();
     }
 }
